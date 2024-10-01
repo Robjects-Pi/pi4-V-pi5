2d40c748b3c38f41d6eec769f95051a369c2b393
garzarobm <garzarobm@gmail.com>
2024-10-01 05:48:53 -0500

    chore: Update benchmark documentation with next steps

--- benchmarks/0.0_benchmark/benchmark_tool.sh
+++ benchmarks/0.0_benchmark/benchmark_tool.sh
@@ -1,61 +1,35 @@
 #!/bin/bash
-echo "Tom's Hardware Benchmark Tool"
-output_file="benchmark_0.csv"
 
+output_file="benchmark.csv"
+
+case "$1" in
+  idle)
+    echo "Idle data for 60 seconds"
+    duration=60
+    ;;
+  stress)
+    echo "Stress data for 300 seconds"
+    duration=300
+    ;;
+  cooldown)
+    echo "Cool down data for 60 seconds"
+    duration=60
+    ;;
+  *)
+    echo "Usage: $0 {idle|stress|cooldown}"
+    exit 1
+    ;;
+esac
 
 echo "Timestamp,CPU Temperature (°C),CPU Clock Speed (MHz),CPU Throttled" > "$output_file"
-echo "Idle data for 60 seconds"
-for i in {1..60}; do
-    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-    echo "$timestamp"
-    cpu_temp=$(vcgencmd measure_temp | cut -d= -f2 | cut -d\' -f1)
-    echo "$cpu_temp"
-    cpu_clock_speed=$(($(vcgencmd measure_clock arm | awk -F= '{print $2}') / 1000000))
-    echo "$cpu_clock_speed"
-    throttled_status=$(vcgencmd get_throttled)
-    echo "$throttled_status"
-    echo "################"
-    echo "$timestamp,$cpu_temp,$cpu_clock_speed,$throttled_status" >> "$output_file"
 
-    # Sleep for 1 second
-    sleep 1
-done
-stress --cpu 4 -t 300 &
-
-echo "Stress data for 300 seconds"
-for i in {1..300}; do
-    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-    echo "$timestamp"
-    cpu_temp=$(vcgencmd measure_temp | cut -d= -f2 | cut -d\' -f1)
-    echo "$cpu_temp"
-    cpu_clock_speed=$(($(vcgencmd measure_clock arm | awk -F= '{print $2}') / 1000000))
-    echo "$cpu_clock_speed"
-    throttled_status=$(vcgencmd get_throttled)
-    echo "$throttled_status"
-    echo "################"
-    echo "$timestamp,$cpu_temp,$cpu_clock_speed,$throttled_status" >> "$output_file"
-
-    # Sleep for 1 second
-    sleep 1
+for i in $(seq 1 $duration); do
+  timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  cpu_temp=$(vcgencmd measure_temp | cut -d= -f2 | cut -d\' -f1)
+  cpu_clock_speed=$(($(vcgencmd measure_clock arm | awk -F= '{print $2}') / 1000000))
+  throttled_status=$(vcgencmd get_throttled)
+  echo "$timestamp,$cpu_temp,$cpu_clock_speed,$throttled_status" >> "$output_file"
+  sleep 1
 done
 
-echo "Stress test complete, 60 second cool down period"
-
-echo "Cool down data for 60 seconds"
-for i in {1..60}; do
-    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-    echo "$timestamp"
-    cpu_temp=$(vcgencmd measure_temp | cut -d= -f2 | cut -d\' -f1)
-    echo "$cpu_temp"
-    cpu_clock_speed=$(($(vcgencmd measure_clock arm | awk -F= '{print $2}') / 1000000))
-    echo "$cpu_clock_speed"
-    throttled_status=$(vcgencmd get_throttled)
-    echo "$throttled_status"
-    echo "################"
-    echo "$timestamp,$cpu_temp,$cpu_clock_speed,$throttled_status" >> "$output_file"
-
-    # Sleep for 1 second
-    sleep 1
-done
-Les Pounder
-
+echo "Data collection complete. Results saved in $output_file"
